ba4b228e62e5cd6ba9fcc2c88759b734c88a1351
imkaadarsh <aadarshkavita@gmail.com>
2022-11-29 13:59:18 +0530

    State Full Widget Using Simple App

--- lib/main.dart
+++ lib/main.dart
@@ -9,11 +9,44 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: "Strix Test",
-        home: Scaffold(
-          appBar: AppBar(title: Text("Strix Test")),
-          body: MyTable(),
-        ));
+      debugShowCheckedModeBanner: false,
+      title: "Strix Test",
+      home: SimpleForm(),
+    );
+  }
+}
+
+class SimpleForm extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _SimpleFormState();
+  }
+}
+
+class _SimpleFormState extends State<SimpleForm> {
+  String name = "";
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(title: Text("Strix Test")),
+      body: Container(
+        child: Column(
+          children: <Widget>[
+            TextField(
+              onSubmitted: (String username) {
+                setState(() {
+                  name = username;
+                });
+              },
+            ),
+            Text(
+              "Name is $name",
+              textDirection: TextDirection.ltr,
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
