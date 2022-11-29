2f39c615a54e561fa2dcaecf8860048525f9a39c
imkaadarsh <aadarshkavita@gmail.com>
2022-11-29 12:55:14 +0530

    forth

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,20 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(Material(
-    color: Colors.purple,
-    child: Center(
-        child: Text(
-      "Aadarsh",
-      textDirection: TextDirection.ltr,
-      style: TextStyle(color: Colors.white, fontSize: 50.8),
-    )),
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    title: "Strix Test",
+    home: Scaffold(
+      appBar: AppBar(title: Text("Strix Test")),
+      body: Material(
+        color: Colors.purple,
+        child: Center(
+            child: Text(
+          "Aadarsh",
+          textDirection: TextDirection.ltr,
+          style: TextStyle(color: Colors.white, fontSize: 50.8),
+        )),
+      ),
+    ),
   ));
 }
