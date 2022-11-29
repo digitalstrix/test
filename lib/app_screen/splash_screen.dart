a5d71cbb2d874927efa43ba7dcd548e4fb65cb9b
imkaadarsh <aadarshkavita@gmail.com>
2022-11-29 18:00:16 +0530

    Splash Screen Code

---
+++ lib/app_screen/splash_screen.dart
@@ -0,0 +1,36 @@
+import 'dart:async';
+
+import 'package:app/app_screen/detail_page.dart';
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(
+        Duration(seconds: 3),
+        () => {
+              Navigator.of(context).pushReplacement(MaterialPageRoute(
+                  builder: (BuildContext context) => MyPageDetails()))
+            });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Image.asset(
+          'assets/images/logo.png',
+          width: 220,
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
+import 'dart:async';
 import 'package:app/app_screen/bottom_bar.dart';
 import 'package:app/app_screen/detail_page.dart';
 import 'package:app/app_screen/drawer.dart';
 import 'package:app/app_screen/form.dart';
 import 'package:app/app_screen/navigation.dart';
 import 'package:app/app_screen/pages.dart';
+import 'package:app/app_screen/splash_screen.dart';
 import 'package:app/app_screen/table_screen.dart';
 import 'package:app/code/drawer.dart';
 import 'package:flutter/material.dart';
@@ -18,6 +20,6 @@ class HomeScreen extends StatelessWidget {
     return MaterialApp(
         debugShowCheckedModeBanner: false,
         title: "Strix Test",
-        home: MyDrawer());
+        home: SplashScreen());
   }
 }
