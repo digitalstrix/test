3b59be05071c61a473ee2d903e548ec4e2bec3cf
imkaadarsh <aadarshkavita@gmail.com>
2022-11-29 13:10:33 +0530

    Tables

--- lib/main.dart
+++ lib/main.dart
@@ -2,19 +2,45 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    title: "Strix Test",
-    home: Scaffold(
-      appBar: AppBar(title: Text("Strix Test")),
-      body: Material(
-        color: Colors.purple,
-        child: Center(
-            child: Text(
-          "Aadarsh",
-          textDirection: TextDirection.ltr,
-          style: TextStyle(color: Colors.white, fontSize: 50.8),
-        )),
-      ),
-    ),
-  ));
+      debugShowCheckedModeBanner: false,
+      title: "Strix Test",
+      home: Scaffold(
+        appBar: AppBar(title: Text("Strix Test")),
+        body: Material(
+            color: Color.fromRGBO(156, 39, 176, 1),
+            child: Container(
+              margin: EdgeInsets.all(25),
+              child: Table(
+                border: TableBorder.all(),
+                children: [
+                  TableRow(children: [
+                    Text(
+                      "First Name",
+                      style: TextStyle(
+                          fontSize: 30.0, fontWeight: FontWeight.bold),
+                      textAlign: TextAlign.center,
+                    ),
+                    Text(
+                      "Last Name",
+                      style: TextStyle(
+                          fontSize: 30.0, fontWeight: FontWeight.bold),
+                      textAlign: TextAlign.center,
+                    )
+                  ]),
+                  TableRow(children: [
+                    Text(
+                      "Aadarsh",
+                      style: TextStyle(fontSize: 25.0),
+                      textAlign: TextAlign.center,
+                    ),
+                    Text(
+                      "Gupta",
+                      style: TextStyle(fontSize: 25.0),
+                      textAlign: TextAlign.center,
+                    )
+                  ]),
+                ],
+              ),
+            )),
+      )));
 }
